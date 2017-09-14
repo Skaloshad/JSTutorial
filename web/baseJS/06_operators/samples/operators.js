fb29ad3f7d8e40122cafa6edf866e909868604d7
Danilov <danilov@kampas.ru>
2017-09-14 14:45:51 +0300

    add operatops folder

---
+++ web/baseJS/06_operators/samples/operators.js
@@ -0,0 +1,26 @@
+var x = 1;
+x = -x; //Унарный оператор -
+//alert(x);
+
+var x = 1; y = 2;
+//alert(x - y); //Бинарный оператор -
+
+var str1 = 'first';
+var str2 = 'second';
+//alert(str1 + str2);//+ складывает как числа, так и строки
+//alert(str1 + x);//first1
+//alert(y + str2);//2second
+//alert(2 - '1');//-1
+
+var apples = '3';
+var oranges = '2';
+
+//alert(apples + oranges);//32
+//alert(+apples + +oranges);//5
+
+//alert(1 % 2);//1 Остаток от деления
+
+x++;//Инкремент
+alert(x);
+x--;//Декримент
+alert(x);
