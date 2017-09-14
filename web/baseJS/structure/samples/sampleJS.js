331e64ab385967e5b3bb2e60df6d3f69badeecc2
Danilov <danilov@kampas.ru>
2017-09-14 13:00:30 +0300

    add structure folder

---
+++ web/baseJS/structure/samples/sampleJS.js
@@ -0,0 +1,13 @@
+alert("first alert inline");alert("second alert inline");
+
+alert("first block alert");
+alert("second block alert");
+
+alert("first alert without ;")
+alert("second alert without ;")
+
+alert(3+
+    1+
+2);
+
+[1,2].forEach(alert)
